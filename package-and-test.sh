287e95e61d6c773a19167fd11811dd4dd3b4df7f
Gaspard Deroyan <gaspard.deroyan@gmail.com>
2025-06-06 19:26:55 +0200

    Enhance packaging script to include deletion of existing dist directory, download and extract portable Python, and install dependencies using the extracted Python environment.

--- package-and-test.sh
+++ package-and-test.sh
@@ -3,6 +3,29 @@
 # Exit on any error
 set -e
 
+# If the dist directory exists, delete it
+if [ -d "dist" ]; then
+    rm -rf dist
+    echo "🔍 Deleted dist directory"
+fi
+
+# Download python portable
+curl -L "https://github.com/astral-sh/python-build-standalone/releases/download/20240107/cpython-3.12.1+20240107-aarch64-apple-darwin-install_only.tar.gz" -o python-mac.tar.gz
+echo "🔍 Downloaded python-mac.tar.gz"
+
+# Extract the tar.gz file into python-portable
+mkdir -p python-portable
+tar -xzf python-mac.tar.gz --strip-components=1 -C python-portable
+echo "🔍 Extracted python-mac.tar.gz into python-portable"
+
+# Delete the tar.gz file
+rm python-mac.tar.gz
+echo "🔍 Deleted python-mac.tar.gz"
+
+# Install the dependencies using the python-portable
+python-portable/bin/python3 -m pip install -r requirements.txt
+echo "🔍 Installed the dependencies using the python-portable"
+
 echo "🚀 Starting packaging process with electron-builder..."
 
 # Run electron-builder. It will create the DMG in the 'dist' folder.
